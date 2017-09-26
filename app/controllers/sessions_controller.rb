cda656923966c143d9967ffa0617fec69ae006a8
ian-ramos <ramos.ianc@gmail.com>
2017-09-26 11:20:31 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if !params[:name] || params[:name] == "" #checking if name is nil or empty
+      redirect_to sessions_new_path
+    else
+      session[:name] = params[:name]
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to '/'
+  end
+
+end
